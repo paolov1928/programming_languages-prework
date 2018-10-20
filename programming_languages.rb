0ac494f86f0f2f3ae2f4d83303cd0a7deefb873a
Paolo Ventura <ventura.paolo@gmail.com>
2018-10-20 13:52:11 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,26 @@
+require 'pry'
+
+
+class ::Hash
+    def deep_merge(second)
+        merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : v2 }
+        self.merge(second, &merger)
+    end
+end
+
 def reformat_languages(languages)
-  # your code here
+  h = Hash.new
+  a = Array.new
+  languages.each do |style, values|
+    h.merge!(values)
+  end 
+  languages.each do |style, values|
+    values.each do |language, language_characteristics|
+    a << [language,[:style, [style]]]
+end
+end
+  a2 = Hash[a.map { |k0, (k, v)| [k0, {k => v}] }]
+final = h.deep_merge(a2)
+final[:javascript][:style]=[:oo, :functional]
+final
 end
